87505581af78929371453596afd4ef4fc3694a23
Merle Breitkreuz <merle@monadic.xyz>
2019-05-22 14:49:23 +0200

    Remove commented out code

--- local-testing.js
+++ local-testing.js
@@ -16,7 +16,6 @@ const run = async () => {
   var machineId = "monadic/radicle/issue";
   var machine = new Machine(machine, machineId)
   var payload = {":body": "testing", ":comments": [], ":created-at": "2019-04-26T08:18:48Z", ":labels": [], ":modified-at": "2019-04-26T08:18:48Z", ":state": ":open", ":title": "Testing"};
-  // var res = machine.query("(list-rsms)");
   var res = machine.sendUnsignedCommand("anonymous-create-issue", payload)
   var hhh = (re) => {console.log(re)};
   request(res,hhh);
